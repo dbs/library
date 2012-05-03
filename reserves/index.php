ac177b8f47b96fdff01f6474e253f3790ac8f00d
Dan Scott <dscott@laurentian.ca>
2012-05-03 16:21:53 -0400

    Update reference from "Circulation Desk" to "Access Services"
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- reserves/index.php
+++ reserves/index.php
@@ -41,7 +41,7 @@
             <p>Professors may place library material which is in high demand on
             reserve. By placing an item on reserve, a professor ensures that the
             maximum number of students will have access to the material.</p>
-            <p>Reserve material is kept at the circulation desk and is available on a
+            <p>Reserve material is kept at the Access Services Desk and is available on a
             limited-term loan period from two hours to three days.</p>
             <p>To access a list of reserves for a given course, please select the
             course below. You can sort the list either by course or instructor by
@@ -52,14 +52,14 @@
             copy summary for that book as outlined in red in the images below. The
             call number may just contain the reserve number, or the reserve number
             followed by the book's actual call number. Ask for a reserve at the
-            circulation desk using this reserve number.</p>
+            Access Services Desk using this reserve number.</p>
           </div>
 
           <div id="instructions_french" style="display:none">
             <p>Les professeurs peuvent mettre en réserve des documents très en
             demande. Les professeurs placent des documents en réserve pour y garantir
             l'accès à un maximum d’étudiants.</p>
-            <p>Ces documents sont conservés au comptoir du prêt et peuvent être
+            <p>Ces documents sont conservés au Comptoir du services d'accès et peuvent être
             empruntés pour une période de deux heures à trois jours.</p>
             <p>Pour accéder aux documents en réserve, sélectionnez votre cours parmi
             la liste si-dessous. Vous pouvez mettre la liste en order alphabétique par
@@ -68,7 +68,7 @@
             apparaîtra dans le catalogue. Pour obtenir le numéro de réserve, cliquez
             sur le titre du livre. Un numéro de réserve se trouve où la cote du livre
             apparait normalement (indiqué en rouge si-dessous). C'est ce numéro de
-            réserve qui doit être demandé au comptoir du prêt afin d'obtenir votre
+            réserve qui doit être demandé au Comptoir du services d'accès afin d'obtenir votre
             document.</p>
           </div>
 
