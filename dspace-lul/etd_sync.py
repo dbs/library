eadd5bde648819c9b4b3a8ed5367fc97b4ab9ddb
Dan Scott <dscott@laurentian.ca>
2014-07-28 10:54:12 -0400

    Create a publisher field for theses if we don't supply one
    Also update the script for the variation on Masters / Master's...
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- dspace-lul/etd_sync.py
+++ dspace-lul/etd_sync.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
+"""
+A script which searches a DSpace database for any items under
+collections named "Masters Theses", "Doctoral Theses", and "Undergraduate
+Theses", and adds them to a top level Electronic Theses community
 
-# etd_sync
-#
-# A script which searches a DSpace database for any items under
-# collections named "Masters Theses", "Doctoral Theses", and "Undergraduate
-# Theses", and adds them to a top level Electronic Theses community
-#
-# Best run as a cronjob
+Best run as a cronjob
+"""
 
 import psycopg2
 
@@ -16,6 +15,7 @@ MASTERS_ID = '104'
 DOCTORAL_ID = '105'
 
 # database settings
+# XXX switch to using .pgpass
 DB = {}
 DB['USER'] = "my_db_user"
 DB['PASSWORD'] = "my_db_password"
@@ -23,6 +23,7 @@ DB['NAME'] = "my_db_name"
 
 
 masters = {'collection': 'Masters Theses', 'id': MASTERS_ID}
+masters2 = {'collection': "Master's Theses%", 'id': MASTERS_ID}
 undergrad = {'collection': 'Undergraduate Theses', 'id': UNDERGRAD_ID}
 doctoral = {'collection': 'Doctoral Theses', 'id': DOCTORAL_ID}
 
@@ -40,7 +41,7 @@ def get_items(type):
   return items
 
 def sync_items(items, type):
-  #our items should be in the form of a list of tuples
+  "our items should be in the form of a list of tuples"
   if not items:
     return None
 
@@ -53,13 +54,37 @@ def sync_items(items, type):
     cur.execute(item_insert, (type['id'], i[0],))
   conn.commit()
 
+def add_publisher():
+  """
+  Add a publisher to any items that don't have one
 
-# Run for undergrad, masters, and doctoral
+  This is a requirement for LAC-BAC as of late 2013.
+  """
 
-undergrad_items = get_items(undergrad)
-masters_items = get_items(masters)
-doctorate_items = get_items(doctoral)
+  try:
+    conn = psycopg2.connect("dbname=dspace user=dspace")
+    cur = conn.cursor()
 
-sync_items(undergrad_items, undergrad)
-sync_items(masters_items, masters)
-sync_items(doctorate_items, doctoral)
+    cur.execute("""
+      INSERT INTO public.metadatavalue (item_id, metadata_field_id, text_value,
+      text_lang) SELECT item_id, '39', 'Laurentian University of Sudbury', 'en_CA'
+      FROM nonpublished;
+    """)
+    conn.commit()
+  except:
+    print("Failed to add publisher metadata value")
+
+if __name__ == '__main__':
+  # Run for undergrad, masters, and doctoral
+
+  undergrad_items = get_items(undergrad)
+  masters_items = get_items(masters)
+  masters2_items = get_items(masters2)
+  doctorate_items = get_items(doctoral)
+
+  sync_items(undergrad_items, undergrad)
+  sync_items(masters_items, masters)
+  sync_items(masters2_items, masters2)
+  sync_items(doctorate_items, doctoral)
+
+  add_publisher()
